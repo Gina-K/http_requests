4ab443d9fd36427a003164e1a20631409ba85ac7
Gina <e.kurmeneva@gmail.com>
2020-12-28 12:37:21 +0300

    Add a custom header to the post request
    And create new .js file.

---
+++ post_header.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const options = {
+    host: 'localhost',
+    path: '/',
+    port: '8080',
+    method: 'POST',
+    headers: {'WhatWillSaveTheWorld': 'Love'}
+};
+
+callback = response => {
+    let info = '';
+    response.on('data', chunk => info += chunk);
+    response.on('end', () => console.log(info));
+}
+
+http.request(options, callback).end();
