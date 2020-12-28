5f9e22cede4517b30a2882ebd4676e2cf402fde3
Gina <e.kurmeneva@gmail.com>
2020-12-28 12:35:09 +0300

    Refactor the server script

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const http = require('http')
-const hostname = '127.0.0.1'
-const port = 8080
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/plain')
-    response.end('Hello there!\n')
+    for (const header in request.headers) {
+        console.log(`${header}: ${request.headers[header]}`);
+    }
+    console.log(`Request to: ${request.url}`);
+    console.log(`Method: ${request.method}`);
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello there!\n');
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, hostname, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
 
-    console.log(`Server running at http://${hostname}:${port}`)
-})
+    console.log(`Server running at http://${hostname}:${port}`);
+});
