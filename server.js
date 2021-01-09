89eddc5ad8dd76a203a9136c817c4bafffeae726
Eugenia Kurmeneva <e.kurmeneva@gmail.com>
2021-01-09 20:05:21 +0300

    Make server greets users by names

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const url = require('url');
 const hostname = '127.0.0.1';
 const port = 8080;
+const names = [];
 
 const requestHandler = (request, response) => {
+    const queryObject = url.parse(request.url, true).query;
+
     console.log(`Request to: ${request.url}`);
     console.log(`Method: ${request.method}`);
 
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello there!\n');
+
+    if (queryObject.name) {
+        names.push(queryObject.name);
+    }
+
+    response.end(names.length ? `Hello, ${names.join(', ')}` : 'Hi there!\n');
 }
 
 const server = http.createServer(requestHandler);
