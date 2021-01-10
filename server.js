8c763cc15f1f7596996f8775be33f60e608a0865
Eugenia Kurmeneva <e.kurmeneva@gmail.com>
2021-01-10 17:22:23 +0300

    Make the server write users' names to the file and read it

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 8080;
-const names = [];
+const dbName = "names.json";
+let names = [];
+
+if (fs.existsSync(dbName)) {
+    names = JSON.parse(fs.readFileSync(dbName, 'utf8'));
+    console.log('>>> names read from file:', names);
+}
 
 const requestHandler = (request, response) => {
     const queryObject = url.parse(request.url, true).query;
@@ -15,6 +22,11 @@ const requestHandler = (request, response) => {
 
     if (queryObject.name) {
         names.push(queryObject.name);
+        fs.writeFile(dbName, JSON.stringify(names), (err) => {
+            if (err) {
+                throw err;
+            }
+        });
     }
 
     response.end(names.length ? `Hello, ${names.join(', ')}` : 'Hi there!\n');
@@ -27,5 +39,5 @@ server.listen(port, hostname, (err) => {
         return console.log('something bad happened', err)
     }
 
-    console.log(`Server running at http://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}!`);
 });
