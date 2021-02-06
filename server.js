e518a70097bab0d222b54a904529dea1af8daa06
gina <e.kurmeneva@gmail.com>
2021-02-06 17:15:27 +0300

    HW5. Add example of bearer strategy for one user

--- server.js
+++ server.js
@@ -4,27 +4,47 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const BearerStrategy = require('passport-http-bearer');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 mongoose.connect('mongodb://localhost:27017');
-const UserSchema = mongoose.Schema({name: String, ip: String});
+const UserSchema = mongoose.Schema({name: String, ip: String, jwt: String});
 const User = mongoose.model('Users', UserSchema);
 
 const localStrategy = new LocalStrategy(
-{ usernameField: "username", passwordField: "pwd"},
+    {usernameField: "username", passwordField: "pwd"},
     (username, password, done) => {
-    done(null, { name: "pavel" });
+        User.find({name: username})
+            .exec()
+            .then(foundUsers => {
+                if (!foundUsers || !foundUsers.length) {
+                    done("Not found");
+                } else {
+                    done(null, foundUsers[0]);
+                }
+            });
     }
 );
 
+const bearerStrategy = new BearerStrategy((token, done) => {
+    done(null, "pavel");
+});
+
 passport.serializeUser((user, done) => {
-    done(null, user);
+    const jwt = "sfdlkjljsdffgf";
+    User.updateOne(
+        {name: user.name},
+        {jwt},
+        (err, updatedUser) => {
+            done(null, jwt);
+        }
+    );
 });
 passport.deserializeUser((user, done) => {
     done(null, user);
-})
+});
 
 const printReqProperties = (req, res, next) => {
     console.log(`Request to: ${req.url}`);
@@ -84,6 +104,7 @@ app.get('/', getHandler);
 app.post('/', checkAuthorisation);
 app.use(bodyParser.json());
 passport.use("local", localStrategy);
+passport.use("bearer", bearerStrategy);
 app.use(passport.initialize());
 
 app.post("/token", passport.authenticate("local", {
@@ -91,7 +112,7 @@ app.post("/token", passport.authenticate("local", {
     failureRedirect: "/failure"
 }));
 
-app.use(postAuthorizedHandler);
+app.use(passport.authenticate("bearer", {session: false}), postAuthorizedHandler);
 
 app.use(errorHandling);
 
@@ -100,7 +121,7 @@ app.listen(port, hostname, () => {
     User.find({}, (err, users) => {
         console.log(
             'In the collection at the moment: ',
-            users.map(u => u.name).join(", ")
+            users.map(u => u.name + " " + u.jwt).join(", ")
         );
     })
 });
